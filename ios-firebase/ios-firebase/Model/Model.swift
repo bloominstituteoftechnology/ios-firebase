c522611a3a6ddc45abaadeab26a3c07a5c255fc8
Ben Hakes <bhhakes@gmail.com>
2018-12-06 15:41:42 -0600

    add new networking-based model methods

--- ios-firebase/ios-firebase/Model/Model.swift
+++ ios-firebase/ios-firebase/Model/Model.swift
@@ -15,8 +15,57 @@ class Model {
         return persons[index]
     }
     
+    // MARK: Core Database Management Methods
+    
+    // TODO: remove this when addNewPerson is complete and working
     func add(person: Person) {
         persons.append(person)
         delegate?.modelDidUpdate()
+    }
+    
+    func addNewDevice(person: Person, completion: @escaping () -> Void) {
+        
+        let person = person
+        
+        // append it to our devices array, updating our local model <-- local
+        persons.append(person)
+        
+        // save it by pushing it to the firebase thing <-- remote
+        Firebase<Person>.save(item: person){ success in
+            guard success else {return}
+            DispatchQueue.main.async { completion()}
+        }
+        
+    }
+    
+    func deletePerson(at indexPath: IndexPath, completion: @escaping () -> Void) {
+        //
+        let person = persons[indexPath.row]
+        
+        //
+        persons.remove(at: indexPath.row)
+        
+        // remote
+        Firebase<Person>.delete(item: person){ success in
+            guard success else {return}
+            DispatchQueue.main.async { completion()}
+        }
+        
+    }
+    
+    
+    func updatePerson(at indexPath: IndexPath, completion: @escaping () -> Void) {
+        //
+        let person = persons[indexPath.row]
+        
+        // TODO: do we need this?
+        //device.uuid = UUID().uuidString
+        
+        // remote
+        Firebase<Person>.save(item: person){ success in
+            guard success else {return}
+            DispatchQueue.main.async { completion()}
+        }
+        
     }
 }
--- ios-firebase/ios-firebase/ViewControllerAndCells/EntryCell.swift
+++ ios-firebase/ios-firebase/ViewControllerAndCells/EntryCell.swift
@@ -8,11 +8,11 @@ class EntryCell: UITableViewCell {
     @IBOutlet weak var cohortField: UITextField!
     
     @IBAction func add(_ sender: Any) {
-        guard let name = nameField.text, !name.isEmpty else { return }
-        let cohort = cohortField.text ?? ""
-        let person = Person(name: name, cohort: cohort)
+        guard let name = nameField.text, !name.isEmpty else { return } // name has to be filled out
+        let cohort = cohortField.text ?? "" // cohort is optional
+        let person = Person(name: name, cohort: cohort) // create a new person instance
         Model.shared.add(person: person)
-        nameField.text = nil
-        cohortField.text = nil
+        nameField.text = nil // reset the name field to be blank
+        cohortField.text = nil // reset the cohort field to be blank
     }
 }

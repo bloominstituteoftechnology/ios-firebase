cd670f3314e3489e5c30860e77d9a40df0bfa7a2
Ben Hakes <bhhakes@gmail.com>
2018-12-06 15:20:38 -0600

    update person class to conform to FirebaseItem protocol and by Codable

--- ios-firebase/ios-firebase/Model/Person.swift
+++ ios-firebase/ios-firebase/Model/Person.swift
@@ -1,8 +1,10 @@
 import Foundation
 
-class Person {
+class Person: Codable, FirebaseItem {
+    
     var name: String
     var cohort: String
+    var recordIdentifier: String = ""
     
     init(name: String, cohort: String) {
         (self.name, self.cohort) = (name, cohort)

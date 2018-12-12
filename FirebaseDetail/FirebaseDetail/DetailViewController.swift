b6535b03feef34c6e37bedcbd65cc2a1dec42e88
Sameera Leola <sameeraleola@gmail.com>
2018-12-12 17:52:30 -0600

    Completed table view layout and constraints

---
+++ FirebaseDetail/FirebaseDetail/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  FirebaseDetail
+//
+//  Created by Sameera Leola on 12/12/18.
+//  Copyright © 2018 Sameera Leola. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}

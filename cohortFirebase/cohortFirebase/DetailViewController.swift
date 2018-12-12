c4a195747cabaa6618540457cb7ec227a9e6c82d
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2018-12-12 17:20:45 -0500

    add necessary file for project

---
+++ cohortFirebase/cohortFirebase/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  cohortFirebase
+//
+//  Created by Julian A. Fordyce on 12/12/18.
+//  Copyright © 2018 Julian A. Fordyce. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

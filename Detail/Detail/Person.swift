337a705c045a8a25062a0757c761884452fbc022
Frulwinn <frulwinn@gmail.com>
2018-12-12 13:51:12 -0800

    add recordIdentifier to Person.swift so FirebaseItem can be a type

--- Detail/Detail/Person.swift
+++ Detail/Detail/Person.swift
@@ -1,8 +1,10 @@
 import Foundation
 
 class Person: Codable, FirebaseItem {
+    
     var name: String
     var cohort: String
+    var recordIdentifier = ""
     
     init(name: String, cohort: String) {
         (self.name, self.cohort) = (name, cohort)

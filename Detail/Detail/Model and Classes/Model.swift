1364193c5b0be03cb76e2d8dc977cc8685a49a05
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-12-12 19:52:57 -0800

    prepare the model for further Firebase integration

--- Detail/Detail/Model and Classes/Model.swift
+++ Detail/Detail/Model and Classes/Model.swift
@@ -15,8 +15,39 @@ class Model {
         return persons[index]
     }
     
-    func add(person: Person) {
+    func addNewPerson(person: Person, completion: @escaping () -> Void) {
         persons.append(person)
         delegate?.modelDidUpdate()
+        
+        Firebase<Person>.save(item: person) { success in
+            guard success else { return }
+            DispatchQueue.main.async {
+                completion ()
+            }
+        }
     }
+
+    func deletePerson(at indexPath: IndexPath, completion: @escaping () -> Void) {
+        let person = persons[indexPath.row]
+        persons.remove(at: indexPath.row)
+
+        Firebase<Person>.delete(item: person) { success in
+            guard success else { return }
+            DispatchQueue.main.async {
+                completion()
+            }
+        }
+    }
+    
+    func updatePerson(at indexPath: IndexPath, completion: @escaping () -> Void) {
+        let person = persons[indexPath.row]
+        
+        Firebase<Person>.save(item: person) { success in
+            guard success else { return }
+            DispatchQueue.main.async {
+                completion()
+            }
+        }
+    }
+
 }

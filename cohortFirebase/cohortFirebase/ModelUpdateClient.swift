714c822f252ac67343dd44441090ddd009ad9524
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2018-12-13 10:28:48 -0500

    implement the ability to use Firebase

---
+++ cohortFirebase/cohortFirebase/ModelUpdateClient.swift
@@ -0,0 +1,9 @@
+//
+//  ModelUpdateClient.swift
+//  cohortFirebase
+//
+//  Created by Julian A. Fordyce on 12/13/18.
+//  Copyright © 2018 Julian A. Fordyce. All rights reserved.
+//
+
+import Foundation
